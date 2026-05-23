c34a2608f89f8fba9774f8f4042d290528d472c4
caaskaupdi-dotcom <caaskaupdi@gmail.com>
2026-05-24 01:46:01 +0300

    Update index.js

--- index.js
+++ index.js
@@ -62,4 +62,4 @@ client.on("interactionCreate", async interaction => {
 
 });
 
-client.login(process.env.TOKEN);
+client.login(process.env.TOKEN=MTUwNzg3MjYwNjQ4ODEwMDkzNA.G-xWoE.IqbZzA8z0mSDqFxqHmNotAEqJun3wRtzvyCw50);
